acb4038ef7a9cf5ca0a573b72248943a7b7dc797
강태선 <ts7681@naver.com>
2023-01-04 23:44:27 +0900

    (UPDATE)백준 2529 풀이 완료

--- Python/BaekJoon/2529/sun.py
+++ Python/BaekJoon/2529/sun.py
@@ -7,15 +7,33 @@ answer = []
 visited = [0] * 10
 N = 0
 
-def DFS(L):
+def check(num1, num2, op):
+    if op == '>':
+        if int(num1) > int(num2):
+            return True
+    if op == '<':
+        if int(num1) < int(num2):
+            return True
+    return False
+
+def DFS(L, num):
     if L == N:
+        answer.append(num)
         return
     for i in range(10):
         if (not visited[i]):
-            pass
+            if L == 0 or check(num[L-1], str(i), S[L-1]):
+                visited[i] = 1
+                DFS(L+1, num+str(i))
+                visited[i] = 0
 
 
 if __name__=="__main__":
     T = int(sys.stdin.readline())
     S = list(sys.stdin.readline().split())
     N = len(S) + 1
+
+    DFS(0, '')
+
+    print(answer[-1])
+    print(answer[0])
