28a8d3494ac4b55f53b082b6998428fbaf1c0d47
강태선 <ts7681@naver.com>
2023-01-11 22:23:53 +0900

    (ADD)백준 3020문제

---
+++ Python/BaekJoon/3020/sun.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.stdin = open('../input.txt')
+
+if __name__ == "__main__":
+    N, M = map(int, sys.stdin.readline().split())
+
+    cave = [0] * M
+
+    for i in range(N):
+        tmp = int(sys.stdin.readline())
+        if i % 2 == 0:
+            cave[M - tmp] += 1
+        else:
+            cave[0] += 1
+            cave[tmp] -= 1
+
+    for i in  range(1, M):
+        cave[i] += cave[i-1]
+
+    print(cave)
+    count = 0
+    low = min(cave)
+    for i in cave:
+        if i == low:
+            count += 1
+
+    print(low, count)
